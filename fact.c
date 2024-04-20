d961844517a5071182e8cf287a7c9c4caa4cc55a
Gagan <gn944913@gmail.com>
2024-04-20 10:39:30 +0000

    project files

---
+++ fact.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+factorial()
+ {
+    int i, number, factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d",&number);
+
+    for(i=1; i<=number; i++) {
+        factorial = factorial * i;
+    }
+
+    printf("Factorial of %d is: %d",number, factorial);
+  #// return 0;
+}
